0fcbafe9ccf52ce016678bcdc119ba9a6243ceab
Sakshi Sahu <sakshi06sahu@gmail.com>
2023-06-05 19:08:59 +0530

    Resolved

--- Sakshi.java
+++ Sakshi.java
@@ -1,6 +1,7 @@
 public class Sakshi{
     public static void main(String[] args){
         System.out.println("Hello");
-        System.out.print("Sakshi");
+        System.out.println("Sakshi");
+        System.out.println("Bug Modified!!!");
     }
 }
