ce35481a8526bdd857d090c4eb87d027cafd85d4
Sakshi Sahu <sakshi06sahu@gmail.com>
2023-06-05 18:55:13 +0530

    Changed code output

--- Sakshi.java
+++ Sakshi.java
@@ -1,5 +1,6 @@
 public class Sakshi{
     public static void main(String[] args){
-        System.out.print("Hello");
+        System.out.println("Hello");
+        System.out.print("Sakshi");
     }
 }
